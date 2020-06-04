b9107bd6c0e02a57584df78b4ba0fc8d75c639fe
Chih Sean Hsu <Sean@Sean-Mac.local>
2020-06-05 00:40:16 +0800

    dynamodb complete

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,4 +1,4 @@
-from gevent import monkey
+from gevent import monkey, spawn
 monkey.patch_all()
 
 from .views import routes
@@ -6,12 +6,16 @@ import os
 import sys
 from flask import Flask
 
+from api.logger import get_logger
+
+get_logger('main')
 
 # prevent RecursionError: maximum recursion depth exceeded while calling a Python object
 sys.setrecursionlimit(100000000)
 
 application = Flask(__name__)
 application.register_blueprint(routes)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=80)
--- src/app/api/views.py
+++ src/app/api/views.py
@@ -1,14 +1,14 @@
-import decimal
-import os
 from gevent import monkey, spawn
 monkey.patch_all()
 
 import boto3
 import botocore.session
-
+import os
+import simplejson as json
 from flask import Blueprint, request, current_app as app
-
 from api.logger import get_logger
+
+import time
 
 logger = get_logger(__name__)
 
@@ -18,24 +18,46 @@ routes = Blueprint(
     static_folder='static'
 )
 
+KWARGS_FOR_DYNAMODB = {
+    'region_name': os.environ.get('REGION', 'ap-northeast-1')
+}
+TABLE = os.environ.get('DYNAMODB_TABLE', 'ut_table')
+if os.environ.get('ENV', 'DEV') == 'LOCAL':
+    KWARGS_FOR_DYNAMODB['endpoint_url'] = 'http://dynamodb-local:8000'
+
+session = botocore.session.get_session()
+client = session.create_client(
+    'dynamodb', **KWARGS_FOR_DYNAMODB)
+
 @routes.route('/ping', methods=['GET'])
 def ping() -> (str):
     logger.debug('pong')
     return 'pong'
 
 def get_item(pri: str, sec: int) -> (dict):
-    db = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://dynamodb-local:8000')
-    table = db.Table(os.environ.get('DYNAMODB_TABLE', 'ut_table'))
-    res = table.get_item(Key={
-        'primary': pri,
-        'second': sec
-    })
-    return res
+    data = {}
+    try:
+        res = client.get_item(TableName=TABLE,
+            Key={
+                'primary': {
+                    'S': pri
+                },
+                'second': {
+                    'N': str(sec)
+                }
+            }
+        )
+        data = res.get('Item', {})
+        
+    except Exception as e: 
+        logger.exception(e)
+
+    return data
+
 
 @routes.route('/get_dy/<string:primary>/<int:second>', methods=['GET'])
 def get_dy(primary: str, second: int) -> (str):
-
-    res = spawn(get_item(primary, second))
-    data = res.get('Item', {})
+    data = get_item(primary, second)
     logger.info(data)
+    
     return data
